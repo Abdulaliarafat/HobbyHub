0207a75080683cf36c1243d3fc0c63a8135c3bfb
abdulaliarafat <abdulaliarafat@gmail.com>
2025-05-24 20:25:44 +0600

    impliment button toggole join or no longer

--- src/Layout/GroupLayout.jsx
+++ src/Layout/GroupLayout.jsx
@@ -4,6 +4,9 @@ import { useLoaderData } from 'react-router';
 const GroupLayout = () => {
     const Details=useLoaderData();
     console.log(Details)
+    const GroupDate=new Date(Details.date);
+    const TodayDate=new Date();
+    const Active=GroupDate >= TodayDate;
     return (
         <div className='max-w-4xl mx-auto my-10 '>
             <div className='md:flex gap-5 shadow-2xl rounded-md bg-gradient-to-tl from-gray-50 to-blue-100'>
@@ -17,7 +20,10 @@ const GroupLayout = () => {
                   <p className='font-semibold text-md'>Category : {Details.Category}</p>
                   <p className='font-semibold text-md text-balance'>Description : {Details.description}</p>
                   <p className='font-semibold text-lg'>Date : {Details.date}</p>
-                  <button className='btn w-xs bg-blue-800 text-white hover:rounded-2xl hover:bg-blue-500 mt-5 mr-3'>Join group</button>
+                  {
+                    Active ?  <button className='btn w-xs bg-blue-700 text-white hover:rounded-2xl hover:bg-blue-500 mt-5 mr-3'>Join group</button> :<button className='btn w-xs bg-red-700 text-white hover:rounded-2xl hover:bg-red-500 mt-5 mr-3'>No longer active</button>
+                  }
+                 
                </div>
             </div>
         </div>
